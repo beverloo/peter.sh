d218f37ef0195eca39b51544d838b752d5f85807
Peter Beverloo <peter@chromium.org>
2015-12-03 12:32:50 +0000

    Very bare-boned version of the Push Generator

---
+++ tests/cases/push-generator-sw.js
@@ -0,0 +1,12 @@
+// Copyright 2015 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(clients.claim());
+});
+
---
+++ tests/cases/push-generator.js
@@ -0,0 +1,41 @@
+// Copyright 2015 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+function PushGenerator(requirementsElement) {
+  RequirementsBase.call(this, requirementsElement);
+
+
+  this.addRequirement(PushGenerator.REQUIREMENT_PERMISSION,
+                      'Requires permission to display notifications.');
+  this.addRequirement(PushGenerator.REQUIREMENT_SERVICE_WORKER,
+                      'Requires the Service Worker to be registered.');
+  this.addRequirement(PushGenerator.REQUIREMENT_SUBSCRIPTION,
+                      'Requires a valid push messaging subscription.');
+}
+
+PushGenerator.REQUIREMENT_PERMISSION = 0;
+PushGenerator.REQUIREMENT_SERVICE_WORKER = 1;
+PushGenerator.REQUIREMENT_SUBSCRIPTION = 2;
+
+PushGenerator.prototype = Object.create(RequirementsBase.prototype);
+
+PushGenerator.prototype.registerServiceWorker = function(scope) {
+  navigator.serviceWorker.register(scope + 'push-generator-sw.js', { scope: scope }).catch(function(error) {
+    console.error('Unable to register the service worker: ' + error);
+  });
+
+  var self = this;
+  return navigator.serviceWorker.ready.then(function(serviceWorker) {
+    self.satisfyRequirement(PushGenerator.REQUIREMENT_SERVICE_WORKER);
+    return serviceWorker;
+  });
+};
+
+PushGenerator.prototype.requestPermission = function() {
+  var self = this;
+  Notification.requestPermission(function(status) {
+    if (status == 'granted')
+      self.satisfyRequirement(PushGenerator.REQUIREMENT_PERMISSION);
+  });
+};
