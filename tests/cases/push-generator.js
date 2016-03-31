ee16f20f5159364d92105f946bd8e54d9b837e76
Peter Beverloo <peter@lvp-media.com>
2016-03-31 01:02:49 +0100

    Refactor the push generator's subscription functionality

--- tests/cases/push-generator.js
+++ tests/cases/push-generator.js
@@ -1,4 +1,4 @@
-// Copyright 2015 Peter Beverloo. All rights reserved.
+// Copyright 2016 Peter Beverloo. All rights reserved.
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
@@ -13,9 +13,206 @@ function toBase64Url(arrayBuffer, start, end) {
                .replace(/\//g, '_');
 }
 
+// -------------------------------------------------------------------------------------------------
+
+function SubscriptionGenerator() {}
+
+// Manifest that contains the 'gcm_sender_id' property for GCM authentication.
+SubscriptionGenerator.MANIFEST_FILE = '/push-generator/push-generator-gcm-manifest.json';
+
+// Public NIST P-256 key in uncompressed format. Must be paired with the private key on the server.
+SubscriptionGenerator.PUBLIC_KEY = [
+  0x04, 0x86, 0x23, 0xC1, 0x85, 0xF0, 0x6C, 0x5D, 0x55, 0x1A, 0xD9, 0x19, 0xAA, 0xE9, 0x02, 0x2F,
+  0x43, 0x55, 0xD2, 0x5C, 0x59, 0x86, 0x69, 0x90, 0xAD, 0xF7, 0x2D, 0xD4, 0x22, 0xD8, 0x63, 0xB6,
+  0xCD, 0xEF, 0x33, 0xB1, 0xBB, 0x66, 0x2F, 0x47, 0xE5, 0xE6, 0x20, 0xFF, 0x0E, 0x10, 0x7F, 0xCD,
+  0xA3, 0x4F, 0x8C, 0x65, 0xF4, 0x64, 0x7E, 0x2C, 0xF3, 0x6B, 0xF8, 0x7C, 0x4B, 0x0C, 0xBD, 0xBF,
+  0xFE
+];
+
+// Creates a <link> element having the 'gcm_sender_id' property and adds it to the DOM.
+SubscriptionGenerator.prototype.createManifest = function() {
+  var manifestLink = document.createElement('link');
+  manifestLink.rel = 'manifest';
+  manifestLink.href = SubscriptionGenerator.MANIFEST_FILE;
+
+  document.querySelector('head').appendChild(manifestLink);
+};
+
+// Removes the manifest having the 'gcm_sender_id' from the DOM if it exists.
+SubscriptionGenerator.prototype.removeManifest = function() {
+  var currentManifest = document.querySelector('link[rel="manifest"]');
+  if (currentManifest)
+    currentManifest.parentNode.removeChild(currentManifest);
+};
+
+// Returns a promise that will be resolved with the current subscription if it exists, or null when
+// it doesn't. Won't be rejected unless a storage error occurs.
+SubscriptionGenerator.prototype.getSubscription = function() {
+  return navigator.serviceWorker.ready.then(function(registration) {
+    return registration.pushManager.getSubscription();
+
+  }).then(function(subscription) {
+    // If you're reading this for inspiration on how to subscribe for push - you probably want to
+    // JSON.stringify(subscription) rather than using the getKey() method manually!
+    return {
+      endpoint: subscription.endpoint,
+      p256dh: subscription.getKey('p256dh'),
+      auth: subscription.getKey('auth')
+    };
+  });
+};
+
+// Subscribes from a |source|, which must be one of { 'document', 'service-worker' }, authenticating
+// with the push service using |authentication|, which must be one of { 'public-key', 'sender-id',
+// 'none' }. The |userVisibleOnly| contract may optionally be set. Returns a promise that will be
+// resolved with the subscription once this has completed.
+SubscriptionGenerator.prototype.subscribe = function(authentication, source, userVisibleOnly) {
+  var subscriptionOptions = {};
+
+  this.removeManifest();
+
+  switch (authentication) {
+    case 'public-key':
+      subscriptionOptions.applicationServerKey = SubscriptionGenerator.PUBLIC_KEY;
+      break;
+    case 'sender-id':
+      this.createManifest();
+      break;
+    case 'none':
+      // Nothing to do, since no information has to be provided.
+      break;
+    default:
+      return Promise.reject(new Error('Invalid authentication: ' + authentication));
+  }
+
+  if (userVisibleOnly)
+    subscriptionOptions.userVisibleOnly = true;
+
+  switch (source) {
+    case 'document':
+      return this.subscribeFromDocument(subscriptionOptions);
+    case 'service-worker':
+      return this.subscribeFromWorker(subscriptionOptions);
+    default:
+      return Promise.reject(new Error('Invalid source: ' + source));
+  }
+};
+
+// Subscribes for push using |subscriptionOptions| from the document. Returns a promise that will be
+// resolved with the subscription when this has been completed.
+SubscriptionGenerator.prototype.subscribeFromDocument = function(subscriptionOptions) {
+  return navigator.serviceWorker.ready.then(function(registration) {
+    if (subscriptionOptions.hasOwnProperty('applicationServerKey')) {
+      subscriptionOptions.applicationServerKey =
+          new Uint8Array(subscriptionOptions.applicationServerKey);
+    }
+
+    return registration.pushManager.subscribe(subscriptionOptions);
+  });
+};
+
+// Subscribes for push using |subscriptionOptions| from the Service Worker. Returns a promise that
+// will be resolved with the subscription when this has completed.
+SubscriptionGenerator.prototype.subscribeFromWorker = function(subscriptionOptions) {
+  var self = this;
+
+  return new Promise(function(resolve, reject) {
+    if (!navigator.serviceWorker.controller) {
+      reject(new Error('Subscribing from a Service Worker requires a controlled page. Refresh?'));
+      return;
+    }
+
+    navigator.serviceWorker.controller.postMessage({
+      command: 'subscribe',
+      subscriptionOptions: subscriptionOptions
+    });
+
+    var messageListenerFunction = function(event) {
+      navigator.serviceWorker.removeEventListener('message', messageListenerFunction);
+      switch (event.data.command) {
+        case 'subscribe-success':
+          resolve(self.getSubscription());
+          break;
+        case 'subscribe-failure':
+          reject(new Error('Subscription failed: ' + event.data.message));
+          break;
+        default:
+          reject(new Error('Invalid command: ' + event.data.command));
+          break;
+      }
+    };
+
+    navigator.serviceWorker.addEventListener('message', messageListenerFunction);
+  });
+};
+
+// Unsubscribes any existing push subscription from the |source|, which must be either 'document' or
+// 'service-worker'. Returns a promise that will be resolved when unsubscription has finished.
+SubscriptionGenerator.prototype.unsubscribe = function(source) {
+  switch (source) {
+    case 'document':
+      return this.unsubscribeFromDocument();
+    case 'service-worker':
+      return this.unsubscribeFromWorker();
+    default:
+      return Promise.reject(new Error('Invalid source: ' + source));
+  }
+};
+
+// Unsubscribes any existing push subscription from the document. Returns a promise that will be
+// resolved when unsubscription has finished.
+SubscriptionGenerator.prototype.unsubscribeFromDocument = function() {
+  return navigator.serviceWorker.ready.then(function(registration) {
+    return registration.pushManager.getSubscription();
+
+  }).then(function(subscription) {
+    if (subscription)
+      return subscription.unsubscribe();
+  });
+};
+
+// Unsubscribes any existing push subscription from the Service Worker. Returns a promise that will
+// be resolved when unsubscription has finished.
+SubscriptionGenerator.prototype.unsubscribeFromWorker = function() {
+  return new Promise(function(resolve, reject) {
+    if (!navigator.serviceWorker.controller) {
+      reject(new Error('Unsubscribing from a Service Worker requires a controlled page. Refresh?'));
+      return;
+    }
+
+    navigator.serviceWorker.controller.postMessage({ command: 'unsubscribe' });
+
+    var messageListenerFunction = function(event) {
+      navigator.serviceWorker.removeEventListener('message', messageListenerFunction);
+      switch (event.data.command) {
+        case 'unsubscribe-success':
+          resolve();
+          break;
+        case 'unsubscribe-failure':
+          reject(new Error('Unsubscription failed: ' + event.data.message));
+          break;
+        default:
+          reject(new Error('Invalid command: ' + event.data.command));
+          break;
+      }
+    };
+
+    navigator.serviceWorker.addEventListener('message', messageListenerFunction);
+  });
+};
+
+
+// -------------------------------------------------------------------------------------------------
+
 function PushGenerator(requirementsElement, element) {
   NotificationGeneratorBase.call(
       this, requirementsElement, element, 'push-generator-sw.js');
+
+  // Class responsible for managing subscriptions with the push service.
+  this.subscriptionGenerator_ = new SubscriptionGenerator();
+
+  // Element that can be displayed when an asynchronous operation is in progress.
+  this.spinnerElement_ = document.getElementById('spinner');
 
   this.unsubscribeElement_ = null;
   this.subscribeElement_ = null;
@@ -27,127 +224,74 @@ function PushGenerator(requirementsElement, element) {
 
 PushGenerator.REQUIREMENT_SUBSCRIPTION = 0;
 
-PushGenerator.GCM_MANIFEST_FILE = '/push-generator/push-generator-gcm-manifest.json';
 PushGenerator.MESSAGE_TARGET = '/push-generator/send-message.php';
 
 PushGenerator.prototype = Object.create(NotificationGeneratorBase.prototype);
 
-PushGenerator.prototype.unsubscribe = function() {
-  var self = this;
-
-  navigator.serviceWorker.ready.then(function(registration) {
-    return registration.pushManager.getSubscription();
-
-  }).then(function(subscription) {
-    if (subscription)
-      return subscription.unsubscribe();
-
-  }).then(function() {
-    self.updateActionState();
-
-  }).catch(function(error) {
-    alert('Unable to unsubscribe: ' + error);
-
-  });
+// Creates the loading spinner for the Subscription Options section of the page.
+PushGenerator.prototype.createSubscriptionSpinner = function() {
+  if (this.spinnerElement_)
+    this.spinnerElement_.style.visibility = 'visible';
 };
 
+// Removes the loading spinner for the Subscription Options section of the page.
+PushGenerator.prototype.removeSubscriptionSpinner = function() {
+  if (this.spinnerElement_)
+    this.spinnerElement_.style.visibility = 'hidden';
+};
+
+// Reads the values from the Subscription Options section in the form and uses the subscription
+// generator to actually subscribe for push messaging.
 PushGenerator.prototype.subscribe = function() {
   var state = this.computeState(true /* includeDefault */),
       self = this;
 
-  var settings = {
-    authentication: this.getField(state, 'authentication', 'manifest'),
-    source: this.getField(state, 'source', 'document'),
-    userVisibleOnly: this.getField(state, 'userVisibleOnly', true)
-  };
+  var authentication = this.getField(state, 'authentication', 'public-key');
+  var source = this.getField(state, 'source', 'document');
+  var userVisibleOnly = !!this.getField(state, 'userVisibleOnly', true);
 
-  this.subscribeElement_.disabled = true;
-  this.doSubscribe(settings).then(function() {
+  this.createSubscriptionSpinner();
+
+  var request = this.subscriptionGenerator_.subscribe(authentication, source, userVisibleOnly);
+  request.then(function(subscription) {
     self.updateActionState();
-
     document.location.hash =
         self.serialize(self.computeState(false /* includeDefault */));
 
   }).catch(function(error) {
     self.subscribeElement_.disabled = false;
-    alert(error);
-  });
+
+    alert('Unable to subscribe: ' + error);
+
+  }).then(this.removeSubscriptionSpinner.bind(this));
 };
 
-PushGenerator.prototype.doSubscribe = function(settings) {
-  if (settings.source == 'sw')
-    return this.doSubscribeFromServiceWorker(settings);
+// Unsubscribes any existing push subscription from the push service.
+PushGenerator.prototype.unsubscribe = function() {
+  var state = this.computeState(true /* includeDefault */),
+      self = this;
 
-  switch (settings.authentication) {
-    case 'manifest':
-      var currentManifest = document.querySelector('link[rel="manifest"]');
-      if (currentManifest)
-        currentManifest.parentNode.removeChild(currentManifest);
+  var source = this.getField(state, 'source', 'document');
 
-      var manifestLink = document.createElement('link');
-      manifestLink.rel = 'manifest';
-      manifestLink.href = PushGenerator.GCM_MANIFEST_FILE;
+  this.createSubscriptionSpinner();
 
-      document.querySelector('head').appendChild(manifestLink);
-      break;
-    case 'public_key':
-      // TODO: Implement support for public key authentication.
-      return Promise.reject('Support for public key authentication has not been implemented yet.');
-    case 'none':
-      // No authentication to be provided at all.
-      break;
-    default:
-      return Promise.reject('Invalid option for |settings.authentication|: ' + settings.authentication);
-  }
+  this.subscriptionGenerator_.unsubscribe(source).then(function() {
+    self.updateActionState();
 
-  return new Promise(function(resolve, reject) {
-    navigator.serviceWorker.ready.then(function(registration) {
-      registration.pushManager.subscribe({ userVisibleOnly: settings.userVisibleOnly })
-        .then(function(subscription) {
-          resolve();
+  }).catch(function(error) {
+    alert('Unable to unsubscribe: ' + error);
 
-        }, function(error) { reject(error);  });
-
-    }, function(error) { reject(error); });
-  });
+  }).then(this.removeSubscriptionSpinner.bind(this));
 };
 
-PushGenerator.prototype.doSubscribeFromServiceWorker = function(settings) {
-  // TODO: Implement support for subscribing from a Service Worker.
-  return Promise.reject('Subscribing from a Service Worker has not been implemented yet.');
-};
-
+// Displays a dialog box with information about the subscription available to the user.
 PushGenerator.prototype.displaySubscription = function() {
-  return navigator.serviceWorker.ready.then(function(registration) {
-    return registration.pushManager.getSubscription();
+  return this.subscriptionGenerator_.getSubscription().then(function(subscription) {
+    var content = document.getElementById('subscription-info-dialog').cloneNode(true /* deep */);
 
-  }).then(function(subscription) {
-    var content = document.getElementById('subscription-info-dialog').cloneNode(true /* deep */),
-        data = JSON.parse(JSON.stringify(subscription));
-
-    var endpoint = data.endpoint;
-    var p256dh = '[undefined]';
-    var auth = '[undefined]';
-
-    if (data.hasOwnProperty('keys')) {
-      if (data.keys.hasOwnProperty('p256dh'))
-        p256dh = data.keys.p256dh;
-
-      if (data.keys.hasOwnProperty('auth'))
-        auth = data.keys.auth;
-
-    // Firefox doesn't seem to expose the encryption keys in the serialization yet.
-    } else if (subscription.__proto__.hasOwnProperty('getKey')) {
-      try {
-        p256dh = toBase64Url(subscription.getKey('p256dh'));
-        auth = toBase64Url(subscription.getKey('auth'));
-
-      } catch (e) {}
-    }
-
-    content.querySelector('#endpoint').textContent = endpoint;
-    content.querySelector('#p256dh').textContent = p256dh;
-    content.querySelector('#auth').textContent = auth;
+    content.querySelector('#endpoint').textContent = subscription.endpoint;
+    content.querySelector('#p256dh').textContent = toBase64Url(subscription.p256dh);
+    content.querySelector('#auth').textContent = toBase64Url(subscription.auth);
 
     return DisplayDialog(content);
   });
