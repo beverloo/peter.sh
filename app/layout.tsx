b91ba97e0f2bda2604f4c1ba27d38fe561f204b1
Peter Beverloo <pbeverloo@gmail.com>
2025-08-05 21:24:55 +0100

    Remove Tailwind again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 // Copyright 2025 Peter Beverloo. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import './globals.css';
-
 export default function RootLayout(props: React.PropsWithChildren) {
     return (
         <html data-theme="peter" lang="en">
